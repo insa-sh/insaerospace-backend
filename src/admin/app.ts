4b1de81f2940d2817ede7939a54c077b9154dca3
TuperWar2.0 <80649077+BillyTheSecond@users.noreply.github.com>
2025-02-13 18:09:05 +0100

    Changement de l'emplacement du logo

--- src/admin/app.ts
+++ src/admin/app.ts
@@ -5,10 +5,10 @@ export default {
     config: {
         locales: ["fr", "en", "es"],
         auth: { // Replace the Strapi logo in auth (login) views
-            logo: "./public/uploads/logo-insaerospace.png",
+            logo: "./public/local/logo-insaerospace.png",
         },
         menu: { // Replace the Strapi logo in the main navigation
-            logo: "./public/uploads/logo-insaerospace.png",
+            logo: "./public/local/logo-insaerospace.png",
         },
     },
 
