7c5dbf997b066bc71571e63600d87787e73a6255
TuperWar2.0 <80649077+BillyTheSecond@users.noreply.github.com>
2025-02-18 18:59:49 +0100

    Modification de l'emplacement du logo du backend

--- src/admin/app.ts
+++ src/admin/app.ts
@@ -1,14 +1,14 @@
-// import AuthLogo from "./public/uploads/logo-insaerospace.png";
-// import MenuLogo from "./extensions/my-menu-logo.png";
+import AuthLogo from './extensions/logo-insaerospace.png';
+import MenuLogo from './extensions/logo-insaerospace.png';
 
 export default {
     config: {
         locales: ["fr", "en", "es"],
         auth: { // Replace the Strapi logo in auth (login) views
-            logo: "./public/local/logo-insaerospace.png",
+            logo: AuthLogo,
         },
         menu: { // Replace the Strapi logo in the main navigation
-            logo: "./public/local/logo-insaerospace.png",
+            logo: MenuLogo,
         },
     },
 
