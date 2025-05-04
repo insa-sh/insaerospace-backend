62c42192e6fa1876562e5fd2b4215c141db43b91
B <79524955+SHAfoin@users.noreply.github.com>
2025-05-04 17:21:17 +0000

    fix: restaurer l'utilisation de la variable d'environnement APP_URL dans la configuration du serveur

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: "https://insaerospace.insash.org/manage",
+  url: env('APP_URL', 'http://localhost:1337'), // Utilisation de la variable d'environnement APP_URL
   app: {
     keys: env.array('APP_KEYS'),
   },
